89d434dd68bfd70eddc52586dbbfc6103e5c10d2
方宇翔 <fangyuxiang@nasetech.com>
2019-10-17 10:41:58 +0800

    修改启动时间规则，增加启动脚本

--- index.js
+++ index.js
@@ -10,48 +10,53 @@ const transporter = nodemailer.createTransport({
   auth: config.mailAccount,
 })
 
-// 每天9:30 开机并发送邮件提示ECS实例IP
-schedule.scheduleJob({ second: 0, minute: 30, hour: 9 }, function() {
-  child_process.exec(
-    `aliyun ecs StartInstance --InstanceId="${config.ecs.id}"`,
-    (err, stdOut) => {
-      if (err) {
-        console.error(err)
-      }
-      console.log(stdOut)
-    },
-  )
-  setTimeout(() => {
-    // 5分钟后获取实例IP 并发送邮件
+// 周一至周五 9:30 开机并发送邮件提示ECS实例IP
+schedule.scheduleJob(
+  { second: 0, minute: 30, hour: 9, dayOfWeek: new schedule.Range(1, 5) },
+  function() {
     child_process.exec(
-      `aliyun ecs DescribeInstances --InstanceIds="['${config.ecs.id}']" --RegionId="${config.ecs.region}"`,
+      `aliyun ecs StartInstance --InstanceId="${config.ecs.id}"`,
       (err, stdOut) => {
         if (err) {
           console.error(err)
         }
-        try {
-          const result = JSON.parse(stdOut)
-          const ipAddress =
-            result.Instances.Instance[0].PublicIpAddress.IpAddress[0]
+        console.log(stdOut)
+      },
+    )
+    setTimeout(() => {
+      // 5分钟后获取实例IP 并发送邮件
+      child_process.exec(
+        `aliyun ecs DescribeInstances --InstanceIds="['${config.ecs.id}']" --RegionId="${config.ecs.region}"`,
+        (err, stdOut) => {
+          if (err) {
+            console.error(err)
+          }
           const mailOptions = {
             from: `ECS启动通知<${config.mailAccount.user}>`,
             to: config.notifyTo,
-            subject: 'ECS启动成功',
-            text: `实例IP: ${ipAddress}`,
+          }
+          try {
+            const result = JSON.parse(stdOut)
+            const ipAddress =
+              result.Instances.Instance[0].PublicIpAddress.IpAddress[0]
+            mailOptions.subject = 'ECS启动成功'
+            mailOptions.text = `实例IP: ${ipAddress}`
+          } catch (e) {
+            console.error(e)
+            console.error(`解析实例信息失败`)
+            console.error(stdOut)
+            mailOptions.subject = '读取ECS实例信息失败'
+            mailOptions.text = stdOut
           }
           transporter.sendMail(mailOptions).catch(console.error)
-        } catch (e) {
-          console.error(e)
-          console.error(`解析实例信息失败`)
-          console.error(stdOut)
-        }
-      },
-    )
-  }, 5 * 60 * 1000)
-})
+        },
+      )
+    }, 5 * 60 * 1000)
+  },
+)
 
-// 每天7:30 关机
-schedule.scheduleJob({ second: 0, minute: 30, hour: 7 }, function() {
+// 每天19:30 关机
+schedule.scheduleJob({ second: 0, minute: 30, hour: 19 }, function() {
   child_process.exec(
     `aliyun ecs StopInstance --InstanceId="${config.ecs.id}"`,
     (err, stdOut) => {
